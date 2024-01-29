80be1b58a4043858583bbf291116c7fe25b233f4
Ganeshg1313 <ganeshcoder333@gmail.com>
2024-01-29 20:47:41 +0530

    update

--- assets/js/products.js
+++ assets/js/products.js
@@ -11,10 +11,9 @@ class Products{
             let products = data.items;
             products = products.map(item =>{
                 const {title,price} = item.fields;
-                const {id} = item.id;
+                const id = item.id;
                 const image = item.fields.image;
-                category = item.fields.category;
-                category = category.join(" ");
+                const category = item.fields.category;
                 return {title,price,id,image,category};
             })
             return products;
@@ -25,31 +24,46 @@ class Products{
     }    
 }
 
-//display products
-class UI{
-    displayProducts(products){
-        if(products){
-            console.log(products);
-            
-        }
-        else{
-            console.log("Not");
-            
-        }
+class UI {
+    displayProducts(products) {
         let result = '';
         products.forEach(product => {
+            const { title, price,image, category } = product;
+            
+            // Convert the array of categories into a single string
+            const categoryString = Array.isArray(category) ? category.join(' ') : category;
+
             result += `
-            <div class="product">
-                <img src=${product.image} alt="">
-                <p class="product-category">${product.category}</p>
-                <p class="product-name">${product.title}</p>
-                <p class="product-price">${product.price}</p>
-            </div>
-            `
+                <div class="product">
+                    <img src=${image} alt="">
+                    <div class="product-info">
+                        <div class="product-info-left">
+                            <p class="product-category">${categoryString}</p>
+                            <p class="product-name">${title}</p>
+                            <p class="product-price">${price}</p>
+                        </div>
+                        <div class="product-info-right">
+                        <button class="bag-btn" data-id=${product.id}>
+                        <svg class="icon cart-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"/></svg>
+                        Add to cart
+                        </button>
+                        </div>
+                    </div>
+                </div>
+            `;
         });
         productsDOM.innerHTML = result;
     }
 }
+
+//Local Storage
+
+class Storage{
+    static saveProduct(products){
+        localStorage.setItem("product", JSON.stringify(products));
+    }
+}
+
 
 document.addEventListener("DOMContentLoaded",() =>{
     
@@ -60,6 +74,7 @@ document.addEventListener("DOMContentLoaded",() =>{
     // get all products
     products.getProducts().then(products =>{
         ui.displayProducts(products);
+        Storage.saveProduct(products);
     }); 
 
 })
