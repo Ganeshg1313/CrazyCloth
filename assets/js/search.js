8a18d86fc85230bd87bcb1cdaf7c1c642afeadb3
Ganeshg1313 <ganeshcoder333@gmail.com>
2024-02-21 17:53:39 +0530

    update

--- assets/js/search.js
+++ assets/js/search.js
@@ -25,7 +25,7 @@ function search(e){
 }
 
 searchInput.addEventListener("change", e => search(e));
-searchInput.addEventListener("keypress", e => search(e));
+// searchInput.addEventListener("keypress", e => search(e));
 
 
 
